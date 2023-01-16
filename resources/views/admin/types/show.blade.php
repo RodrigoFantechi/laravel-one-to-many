7b54edb59d482f1c83a612301a787064b38a5ce7
RodrigoFantechi <rodrifante595@libero.it>
2023-01-16 16:18:08 +0100

    add show

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -1,3 +1,6 @@
 @extends('layouts.admin')
 @section('content')
+<div class="container text-center p-5 d-flex align-items-center flex-column">
+        <h1 class="mt-4">Type: {{$type->name}}</h1>
+</div>
 @endsection
